d3849a1562d418fb4845d131ebc049011317ee30
Daniel <ahmagensh@gmail.com>
2025-12-28 13:51:14 +0100

    Update service worker cache and fetch logic

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE = "viral-link-v1";
-const ASSETS = [
+const CACHE = "viral-arcade-v2";
+const CORE = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js",
+  "./game.js",
   "./manifest.webmanifest"
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
 });
 
 self.addEventListener("activate", (e) => {
@@ -18,8 +18,16 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+
+  // do not hard-cache video forever; just network-first
+  if (url.pathname.includes("/assets/intro.mp4")) {
+    e.respondWith(fetch(e.request).catch(() => caches.match("./index.html")));
+    return;
+  }
+
   e.respondWith(
-    caches.match(e.request).then((hit) => hit || fetch(e.request).then((res) => {
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
       const copy = res.clone();
       caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
       return res;
